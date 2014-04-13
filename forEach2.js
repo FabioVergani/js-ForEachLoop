c9122f4ab31b2885dea9550727ed79c6a0685682
FabioVergani <fabio.vergani.1975@gmail.com>
2014-04-13 09:49:48 +0200

    Create forEach2.js

---
+++ forEach2.js
@@ -0,0 +1,52 @@
+function isFunction(x){return typeof(x)==='function'};//noTypeConversionHere!
+
+
+//#2
+function forEach(m,f){
+	var r=m.length;
+	if(r && isFunction(f)){
+		for(var i=0,l=r+0;i<l;f(m[i++]));
+		r=1;
+	};
+	return r
+}
+
+//Test:
+var goodaction=function(x){console.log(x)},aliasaction=goodaction,badaction=1000;
+[10,[],[1,2,3,true],{},{a:'b'}].forEach(function(o,i){
+	console.log('#'+i+',',o);
+	console.log('\tgood action sucess? ',forEach(o,goodaction)?'yes':'no');
+	console.log('\talias action sucess? ',forEach(o,aliasaction)?'yes':'no');
+	console.log('\tbad action sucess? ',forEach(o,badaction)?'yes':'no');
+});
+
+/*
+#0, 10
+	good action sucess?  no
+	alias action sucess?  no
+	bad action sucess?  no
+#1, []
+	good action sucess?  no
+	alias action sucess?  no
+	bad action sucess?  no
+#2, [1, 2, 3, true]
+1
+2
+3
+true
+	good action sucess?  yes
+1
+2
+3
+true
+	alias action sucess?  yes
+	bad action sucess?  yes
+#3, Object {}
+	good action sucess?  no
+	alias action sucess?  no
+	bad action sucess?  no
+#4, Object {a: "b"}
+	good action sucess?  no
+	alias action sucess?  no
+	bad action sucess?  no 
+*/
